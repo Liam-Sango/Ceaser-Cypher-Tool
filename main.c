cd88c77ddfd5ef38fbacb75ce748973d162ba053
Liam-Sango <anonymous_aussie42@proton.me>
2025-11-17 12:18:18 +1100

    Started on String Processing function, Added global vars.

--- main.c
+++ main.c
@@ -4,9 +4,17 @@
 #include <string.h>
 #include <stdbool.h>
 
+//GLOBAL VARS
+char L_Eng_alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+char str_to_prc[20] = {};
 //MAIN LOOP
 int main () {
  return 0;
 }
 
 //FUNCTIONS
+char* Process_string_chunk (char chunk[10]) {
+    //Dynamically allocates 
+    char* Processed_Chunk = (char*)calloc(10, sizeof(char));
+    if (Processed_Chunk == NULL) {printf("\nMemory Allocation error in Process_string() function\n"); exit(1);}
+}

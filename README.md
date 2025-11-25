0825319da3852bfa49f42dac54ed65d2e7026f39
Liam-Sango <anonymous_aussie42@proton.me>
2025-11-25 13:25:20 +1100

    Added Function names, finished TODO list

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Write and test create_cypher_table() 
+
+Write and test Process_text_chunk()
+
+Write and test read_text_chunk() 
+
+Write and test write_text_chunk()
+
+
+Write and test Main loop
+
+Polish and finish program
--- main.c
+++ main.c
@@ -5,16 +5,28 @@
 #include <stdbool.h>
 
 //GLOBAL VARS
-char L_Eng_alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-char str_to_prc[20] = {};
-//MAIN LOOP
-int main () {
+char Lowercase_Eng_alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+char str_chunk[10] = {};
+
+int main() {  
+}
+
+
+
+//PROCESSING FUNCTIONS
+char* create_cypher_table () {
  return 0;
 }
 
-//FUNCTIONS
-char* Process_string_chunk (char chunk[10]) {
-    //Dynamically allocates 
-    char* Processed_Chunk = (char*)calloc(10, sizeof(char));
-    if (Processed_Chunk == NULL) {printf("\nMemory Allocation error in Process_string() function\n"); exit(1);}
+char* Process_text_chunk () {
+    return 0;
+}
+
+//FILE HANDLING FUNCTIONS
+char* read_text_chunk () {
+    return 0;
+}
+
+char* write_text_chunk () {
+    return 0;
 }
